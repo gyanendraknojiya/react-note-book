ed0853dd77d94370a7e18f953249d5606828d01b
gyanendraknojiya <gyanendrak064@gmail.com>
2020-07-21 01:40:04 +0530

    Updates

---
+++ precache-manifest.7a7b596990728bbbd5b8da5b499c37fe.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dc3d602c6b11deb2902cf0f12e0e7673",
+    "url": "/react-note-book/index.html"
+  },
+  {
+    "revision": "3e2a9f474d1ecbc554e2",
+    "url": "/react-note-book/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "8102c04ecc17a77c8f9b",
+    "url": "/react-note-book/static/css/main.1cc49a33.chunk.css"
+  },
+  {
+    "revision": "3e2a9f474d1ecbc554e2",
+    "url": "/react-note-book/static/js/2.72409660.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-note-book/static/js/2.72409660.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8102c04ecc17a77c8f9b",
+    "url": "/react-note-book/static/js/main.ea9d8ba9.chunk.js"
+  },
+  {
+    "revision": "fea29cf7b0c5275430e0",
+    "url": "/react-note-book/static/js/runtime-main.0ee6cea6.js"
+  }
+]);
